339289fa19f3bc500d32747ba1fe487ad12e318f
Your name here <elia.orsini@hotmail.com>
2024-09-04 09:06:41 +0100

    chore: github workflow

--- src/utils/parseFilmName.ts
+++ src/utils/parseFilmName.ts
@@ -3,15 +3,20 @@ export default function parseFilmName(filmName: string): string {
 
   filmName = filmName.replace("preview: ", "");
   filmName = filmName.replace("+ q&a", "");
+  filmName = filmName.replace("+ recorded q&a", "");
   filmName = filmName.replace(" (subtitled)", "");
   filmName = filmName.replace(" (dubbed)", "");
   filmName = filmName.replace(/\(.*\)/, "");
   filmName = filmName.replace(/\-.*th anniversary/, "");
   filmName = filmName.replace("youth screening: ", "");
-  filmName = filmName.replace("take 2: ", "");
-  filmName = filmName.replace("visible cinema: ", "");
   filmName = filmName.replace("massive presents - preview:", "");
   filmName = filmName.replace(" + introduction", "");
+
+  // remove all different formats
+  filmName = filmName.replace("take 2: ", "");
+  filmName = filmName.replace("visible cinema: ", "");
+  filmName = filmName.replace("access film club: ", "");
+  filmName = filmName.replace("movie memories: ", "");
 
   return filmName;
 }
